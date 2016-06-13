f663254c1e9e7a92d13ecdcf5d2f8a442b6aef32
tescoint <tescointsite@gmail.com>
2016-06-14 02:12:29 +0800

    Update artist-ids.js

--- util/artist-ids.js
+++ util/artist-ids.js
@@ -6,30 +6,179 @@
 
 module.exports = {
   pop: [
-    262836961   // ADELE
-    , 459885    // Avril Lavigne
-    , 1419227   // Beyoncé
-    , 217005    // Britney Spears
-    , 64387566  // Katy Perry
-    , 277293880 // Lady GaGa
-    , 184932871 // MIKA
+      119258059     //2Face
+    , 274725761     //9ice
+    , 481383337   // Yemi Alade
+    , 973451696   // Adekunle Gold
+    , 26089928    // Asa
+    , 260802864   //Cobhams Asuquo
+    , 158227126   //Banky W
+    , 833611554   //Korede Bello
+    , 419956872   //Brymo
+    , 586786082   //Chidinma
+    , 119240106   //Dbanj
+    , 321873755   //D'Prince
+    , 119240102   //Don Jazzy
+    , 526258226   //Dr Sid
+    , 287712491   //Di'ja
+    , 412032998   //Amarachi
+    , 463183936   //Emma Nyra
+    , 101911760   //Eedris Abdulkareem
+    , 65327058    //Faze
+    , 520417106   //HarrySong
+    , 360504820   //Iyanya
+    , 194605362   //Kcee
+    , 855238148   //Lil Kesh
+    , 480217968   //Mo Cheddah
+    , 901988673   //Genevieve
+    , 932978337   //Monica Ogah
+    , 511284693   //Omawumi
+    , 586848979   //Omotola Jolade
+    , 176621657   //Onyeka Onwenu
+    , 828415607   //Reekado Banks
+    , 309335750   //WhizKid
+    , 418011557   //Waconzy
+    , 324225716   //Tiwa Savage
+    , 321187699   //May7ven
+    , 504190300   //Mr 2kay
+    , 128880985   //Praiz
+    , 531364876   //Runtown
+    , 436265893   //Seyi Shay
+    , 306347382   //Waje
+     ,517329823        //Eva Alordiah
+      , 260136055   //African Boy
+      , 334602885   //Dagrin
+      , 206540188   //Da'Emperor
+      , 192557881   //elDee
+      , 360389344   //Falz
+      , 309335658   //Ice Prince
+      , 385350270   //Jesse jags
+      , 924250671   //M.I.
+      , 26592985    //M Trill
+      , 283943489 //Mode 9
+      , 1042111883  //Muna
+      , 514017875   //Phyno
+      // , 1855044   //Weird Mc
+      , 843234442 //CDQ
+      , 934321806 //Chinko Ekun
+      , 508405356   //Yung6ix
+      , 992144443   //YCEE
+      , 419957057   //Skales
+      , 389401008   //Olamide
+      , 111796431   //RuggedMan
+    // , 262836961   // ADELE
+    // , 459885    // Avril Lavigne
+    // , 1419227   // Beyoncé
+    // , 217005    // Britney Spears
+    // , 64387566  // Katy Perry
+    // , 277293880 // Lady GaGa
+    // , 184932871 // MIKA
   ],
-  rap: [
-    1587965     // A Tribe Called Quest
-    , 1971863   // Beastie Boys
-    , 465802    // Cypress Hill
-    , 384304    // EPMD
-    , 289550    // OutKast
-    , 13503763  // Swollen Members
-    , 43680     // The Roots
+  rapper: [
+      517329823        //Eva Alordiah
+      , 260136055   //African Boy
+      , 334602885   //Dagrin
+      , 206540188   //Da'Emperor
+      , 192557881   //elDee
+      , 360389344   //Falz
+      , 309335658   //Ice Prince
+      , 385350270   //Jesse jags
+      , 924250671   //M.I.
+      , 26592985    //M Trill
+      , 283943489 //Mode 9
+      , 1042111883  //Muna
+      , 514017875   //Phyno
+      // , 1855044   //Weird Mc
+      , 843234442 //CDQ
+      , 934321806 //Chinko Ekun
+      , 508405356   //Yung6ix
+      , 992144443   //YCEE
+      , 419957057   //Skales
+      , 389401008   //Olamide
+      , 111796431   //RuggedMan
+    // 1587965     // A Tribe Called Quest
+    // , 1971863   // Beastie Boys
+    // , 465802    // Cypress Hill
+    // , 384304    // EPMD
+    // , 289550    // OutKast
+    // , 13503763  // Swollen Members
+    // , 43680     // The Roots
+
   ],
-  rock: [
-    5040714     // AC/DC
-    , 462006    // Bob Dylan
-    , 994656    // Led Zeppelin
-    , 3296287   // Queen
-    , 562555    // The Beach Boys
-    , 136975    // The Beatles
-    , 62819     // The Jimi Hendrix Experience
+
+  reggae: [
+    77973263    //Black Face 
+  , 591899010 //Burna Boy
+  , 319054999 //Duncan Mighty
+  , 14749297  //Dr. Alban
+  , 355856842 //Orezi
+  , 772255131 //patoranking
+  , 282132704 // Timaya
+  , 27764109 //Majek Fashek
+  , 337910718 //Sonny Okosuns
+  , 669332055 // Sean Tizzle
+  , 128617624  //Sound Sultan
+  , 792968862   //Tekno Miles
+  , 103082000   //Tony Tetuila
+  , 26948805    //Eddy Wata
+
+  ],
+
+  oldies: [
+    271400130  //Ayinde Bakare
+  , 602606704 //Alhaji Dauda Epo-Akara
+  , 598065737 // Alhaji Alamu Atatalo
+  , 128877660 //Adewale Ayuba
+  , 377085580 //Dele Ojo
+  , 251263103 //Yinka Davies
+  , 79359975  //Haruna Ishola
+  , 270610340 // Peter King
+  , 301680872 // Tunde King
+  , 549022675 // Ayinla Kollington
+  , 55088 //Fela Kuti
+  , 399130  //Femi Kuti
+  , 256287083 //SEun Kuti
+  , 7021236 //Lagbaja
+  , 517178421 //Obesere
+  , 377026818 //Ebenezer Obey
+  , 941431 //Sade Adu
+  , 215589978 //Sina Peters
+  , 531155154 //Salawa beni
+  , 77539 //Sunny Ade
+  , 476683 //Babatunde Olatunji
+  , 634816976 //I.K. Dairo
+  , 472350094 //oliver De Coque
+  , 195150668 //Fela Sowande
+  , 2475847 //Prince Nico
+  , 314253227 //Yusuf Olatunji
+  , 6667947   //William Onyeabor
+  , 470978385 //Orlando owoh
+  , 66506700    //REex Lawson
+  , 589313020   //Tunde Nightingale
+  , 79359590    //Sir Victor Uwaifo
+
   ]
+
+  // gospel:[
+  //   253975953 //Tope Alabi
+  //   , 172278322 //Tony Allen
+  //   , 469277518 // Yinka Ayefele
+  //   , 304569528 //Ty Bello
+  //   , 5401553 //Frank Edwards
+  //   , 376113241 //Nosa
+  //   , 346407731 //Obiwon
+  //   , 176614584 //Victor Olaiya
+  //   , 909258651 // patience Ozokwor
+  //   , 325036607 //Samsong
+  //   , 909258648 //Stella Damasus
+  //   , 128623973 //Niyola
+  //   , 6249833   //Nneka
+  //   , 519026522 //Sinach
+  //   , 251836467  //Tolumide
+
+  // ]
+
+
+
+
 };
